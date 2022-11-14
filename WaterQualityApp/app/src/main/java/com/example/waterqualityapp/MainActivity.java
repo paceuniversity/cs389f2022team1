85e2d056091d9754b256ee6a30e3c7565a9258cb
my76402p <my76402p@pace.edu>
2022-11-14 12:20:47 -0500

    Added another page to the app to represent water quality features when pressing Enter

--- WaterQualityApp/app/src/main/java/com/example/waterqualityapp/MainActivity.java
+++ WaterQualityApp/app/src/main/java/com/example/waterqualityapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.waterqualityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.WaterQualityApp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayButtons.class);
+        EditText editText = (EditText) findViewById(R.id.inputBox);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
 }
