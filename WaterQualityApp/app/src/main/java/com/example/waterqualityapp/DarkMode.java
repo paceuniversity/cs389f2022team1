94e6c647698cfe703d559d9b9d20e321b582414f
my76402p <my76402p@pace.edu>
2022-11-28 12:43:22 -0500

    Dark Mode button is functional.

---
+++ WaterQualityApp/app/src/main/java/com/example/waterqualityapp/DarkMode.java
@@ -0,0 +1,13 @@
+package com.example.waterqualityapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DarkMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dark_mode);
+    }
+}
--- WaterQualityApp/app/src/main/java/com/example/waterqualityapp/DisplayButtons.java
+++ WaterQualityApp/app/src/main/java/com/example/waterqualityapp/DisplayButtons.java
@@ -3,16 +3,26 @@ package com.example.waterqualityapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DisplayButtons extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.WaterQualityApp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_buttons);
     }
 
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DarkMode.class);
+        //EditText editText = (EditText) findViewById(R.id.inputBox);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     // Get the Intent that started this activity and extract the string
     //Intent intent = getIntent();
     //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
